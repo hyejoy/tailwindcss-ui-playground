88e7526089ba513de64d7250a31f6e925d40cfb3
hyejoy <devchoi97@gmail.com>
2026-02-02 09:18:22 +0900

    feat: 12강 컨테이너 레이아웃 및 중앙 정렬 구현
    - container와 mx-auto를 조합하여 콘텐츠 영역 중앙 배치
    - max-w-2xl을 이용한 레이아웃 가로 너비 제한 설정
    - h-screen 및 bg-indigo-600으로 전체 레이아웃 가시화

---
+++ src/app/lecture_12/container/page.tsx
@@ -0,0 +1,16 @@
+/**
+ *
+ * @container : 웹 레이아웃에서 콘텐츠 영역을 깔끔하게 정리하고 중앙에 배치하는데 주로 사용함
+ */
+export default function Page() {
+  return (
+    <>
+      <main className="container mx-auto h-screen max-w-2xl bg-indigo-600 p-4 text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed
+        repudiandae omnis nobis molestias placeat quaerat? Ducimus laborum earum
+        at pariatur totam id corrupti natus. Digni ssimos ipsam facere possimus
+        consequuntur!
+      </main>
+    </>
+  );
+}
