35a824d84636a875d90386357678dc44daf5970f
hyejoy <devchoi97@gmail.com>
2026-02-20 14:45:38 +0900

    chore: install Font Awesome core and React components
    - @fortawesome/fontawesome-svg-core (^7.2.0) 설치
    - @fortawesome/free-solid-svg-icons (^7.2.0) 설치
    - @fortawesome/react-fontawesome (^3.2.0) 설치
    - SVG 기반 아이콘 시스템 및 React 전용 컴포넌트 환경 구축

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
-import "./globals.css";
+import './globals.css';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
